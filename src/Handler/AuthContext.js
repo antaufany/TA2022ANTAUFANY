0ec30303d7376e9e930c76166538d0e1b1aad280
antaufany <antaufanypuji.r@gmail.com>
2022-02-02 02:55:19 +0700

    FIXLULUS

--- src/Handler/AuthContext.js
+++ src/Handler/AuthContext.js
@@ -1,43 +1,41 @@
-import React, {useContext, useEffect,useState} from 'react'
-import {auth} from '../Firebase'
-
+import React, { useContext, useEffect, useState } from "react";
+import { auth, perf } from "../Firebase";
 
 const AuthContext = React.createContext();
-export function useAuth(){
-    return useContext(AuthContext);
+export function useAuth() {
+  return useContext(AuthContext);
 }
+
 export function AuthProvider({ children }) {
+  const trace = perf.trace("queryTrace");
+  trace.start();
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+  function signup(email, password) {
+    const Register = auth.createUserWithEmailAndPassword(email, password);
 
-   
-    const [currentUser, setCurrentUser] = useState();
-    const [loading, setLoading] = useState(true);
-    function signup(email, password) {
-        return auth.createUserWithEmailAndPassword(email, password)
-    }
-    function login(email, password) {
-        return auth.signInWithEmailAndPassword(email, password)
-    }
-    function logout() {
-        return auth.signOut();
-    }
+    return Register;
+  }
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+  function logout() {
+    return auth.signOut();
+  }
+
   useEffect(() => {
-   const unsubscribe = auth.onAuthStateChanged(user =>{
-    setCurrentUser(user)
-    setLoading(false);    
-    
-
-    })
-      return unsubscribe
-  }, [])
-    const value = {
-            currentUser,
-            login,
-            signup,
-            logout
-    }
-    return (
-        <AuthContext.Provider value={value} >
-            {!loading  && children }
-        </AuthContext.Provider>
-    )
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+  const value = {
+    currentUser,
+    login,
+    signup,
+    logout,
+  };
+  trace.stop();
+  return <AuthContext.Provider value={value}>{!loading && children}</AuthContext.Provider>;
 }
